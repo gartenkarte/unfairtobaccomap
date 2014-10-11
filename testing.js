c732b55201b687e430cb82bafd12bb28233ecee9
almereyda <almereyda@gmail.com>
2014-10-11 18:48:27 +0200

    refactor geojson display functions

--- testing.js
+++ testing.js
@@ -10,37 +10,20 @@ L.control.layers(baseLayers).addTo(map);
 
 
 
-function condense_locations ( element ) {
-  var callback = [];
-  callback[0] = unfairtobacco.locations[element].latitude;
-  callback[1] = unfairtobacco.locations[element].longitude;
-  //console.log(callback);
-  return callback;
-}
-
-function replace_locations ( element ) {
-  var copy = $.extend(true, {}, element, copy);
-  copy.locations = $.map(element.locations, condense_locations);
-  return copy;
-}
-
-function api_to_leaflet_layer ( dictionary ) {
-  result = [];
-
-  for (var key in dictionary) {
-    var obj = dictionary[key];
-    if (obj.locations[0] != null) {
-      result.push(L.marker (replace_locations(obj).locations))
+function OnEachFeature(feature, layer){
+    if (feature.properties && feature.properties.title) {
+        layer.bindPopup('<b>' + feature.properties.title + '</b>'
+                        );
+        layer.on('mouseover', function () {
+            this.openPopup();
+                        });
+        layer.on('mouseout', function () {
+            this.closePopup();
+                        });
     }
-/*    for (var prop in obj) {
-      if (obj.hasOwnProperty(prop)){
-        console.log(prop);
-      }
-    } */
-  };
+};
 
-  return result;
-}
+
 
 function condense_geojson_locations ( element ) {
   var callback = [];
@@ -49,7 +32,6 @@ function condense_geojson_locations ( element ) {
   //console.log(callback);
   return callback;
 }
-
 
 function replace_geojson_locations ( element ) {
   var copy = $.extend(true, {}, element, copy);
@@ -84,41 +66,24 @@ function render_to_geojson ( projects ) {
   return geojson_projects;
 }
 
+
+
 var unfairtobacco;
-var projekte_layer;
+var projekte_geojson;
+
 $.getJSON( "data.json", function( data ) {
   
   unfairtobacco = data;
   
-  //projekte_layer = api_to_leaflet_layer(unfairtobacco.projects);
-  //console.log(projekte_layer);
-  
   projekte_geojson = render_to_geojson(unfairtobacco.projects);
   //console.log(projekte_geojson);
 
-  //projects_in_layer = L.layerGroup(projekte_layer);
-  //console.log(projects_in_layer);
-  
-  //projects_in_layer.addTo(map);
-
-    var Layer_project = L.geoJson(projekte_geojson, {
-        onEachFeature: OnEachFeatureFunction
-    }).addTo(map);
+  var Layer_project = L.geoJson(projekte_geojson, {
+      onEachFeature: OnEachFeature
+  }).addTo(map);
 
 });
 
 
 
 
-function OnEachFeatureFunction(feature, marker){
-    if (feature.properties.name) {
-        marker.bindPopup('<b>' + feature.properties.name + '</b>'
-                        );
-        marker.on('mouseover', function () {
-            this.openPopup();
-                        });
-        marker.on('mouseout', function () {
-            this.closePopup();
-                        });
-    }
-};
